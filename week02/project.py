c7ce99b87cea0f78a3ab92803631437c4eeb18bd
ComeauBYUI <comeaul@byui.edu>
2019-04-19 15:51:28 -0600

    Lessons and first 2 weeks

---
+++ week02/project.py
@@ -0,0 +1,85 @@
+"""
+Course: CS101
+File: <name of your file>
+Project: <project number>
+Author: <Your name>
+
+Description:
+  <What does your project/program do?>
+"""
+
+"""
+Instructions:
+
+- Complete the following problems.
+- Remember that coding style is worth 10%
+  - Fill in code header
+  - follow coding style outlined in the course
+
+You must match the following output for full points:
+
+Hello World
+Hello World
+Hello World is length 11
+ABC
+CBA
+ABCBA
+John
+My name is John Brown and I'm 5' 8"
+********** Options ***********
+*                            *
+*        1 - Display         *
+*       2 - Play game        *
+*     3 - Exit and Save      *
+*                            *
+******************************
+
+"""
+
+# Add your code below
+
+
+# Problem 1: TODO -> display "Hello World" using one print() statement
+
+
+# Problem 2: TODO -> display "Hello World" using two print() statements
+
+
+# Problem 3: TODO -> display "Hello World is length 11", using the len() function
+
+
+# Problem 4: TODO -> display "ABC", change the '?' string to get the output using {}
+print('?'.format('A', 'B', 'C'))
+
+
+# Problem 5: TODO -> Complete the following to display "CBA" by replacing "?" with "{}"s
+print('?'.format('A', 'B', 'C'))
+
+
+# Problem 6: TODO -> Complete the following to display "ABCBA" by replacing "?" with "{}"s
+print('?'.format('A', 'B', 'C'))
+
+
+# Problem 7: TODO -> Complete the following to display "John" by replacing "?" with "{}"s
+print('?'.format('J', 'h', 'n', 'o'))
+
+
+# Problem 8: TODO -> Display your name and your height using the format() statement
+# Example: My name is John Brown and I'm 5' 8"
+
+
+# Problem 9: TODO -> Display the following.  You must use the alignment (left, center or right)
+# features of the format() function.
+"""
+Example output:
+
+************ Options ************
+*                               *
+*          1 - Display          *
+*         2 - Play game         *
+*       3 - Exit and Save       *
+*                               *
+*********************************
+
+"""
+
---
+++ week02/team.py
@@ -0,0 +1,40 @@
+"""
+Course: CS101
+File: team02.py
+Author: Brother Comeau
+
+Description:
+  This is the code for the weekly team activity.  
+  Please work together in groups of 2 to 3.  
+  You will not be sumbitting your code for this activity.
+  You are free to continue working on this activity after class if you need more time.
+"""
+
+# TODO -> Display your full name using a print() statement
+
+
+# TODO -> Display your full name using two print() statements, one for
+#         your first name and one for your last name.
+
+
+# TODO -> Create print() statements to output the following three lines;
+#
+# I am a BYU Idaho student
+# Programming is fun!
+# I will be switching my major to Computer Science.
+
+
+
+# TODO -> Display the text "She's great!" using a print() statement
+
+
+# TODO -> Display the text "She said "How are you?" to him" using a print() statement
+
+
+# TODO -> Display the text "1,2,3" using the 'sep' option
+
+
+# TODO -> Display the text "       Hi There" using the format() statement and print()
+#         You should be using the option to right align text within a space of 20 spaces.
+
+# TODO -> Display the following using 
