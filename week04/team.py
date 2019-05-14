4b839db4ef175a18158814e2113438d09efe2bbb
ComeauBYUI <comeaul@byui.edu>
2019-05-14 09:44:46 -0600

    Update to week 04 team activity

--- week04/team.py
+++ week04/team.py
@@ -16,47 +16,72 @@ Create a program to do the following:
 
 - Prompt for the persons's name
 - Prompt for the persons's gpa
-- Prompt for 4 grades between 1 and 100
-- Display the prompted information in the format shown below
+- Prompt for 4 assignment grades between 1 and 100
+- Prompt for 4 test grades between 1 and 100
 - Note: you should test your program with different inputs:
   - long name
   - short name
   - all grades less than 10
   - all grades 100
+- Weighted average percentages:
+  - 1st grade is worth: 20%
+  - 2nd grade is worth: 20%
+  - 3rd grade is worth: 30%
+  - 4th grade is worth: 30%
+- Display the prompted information in the format shown below
+
 
 Sample Output: (Use different values when you test your program)
 
 Enter name: John Brown
 Enter gpa: 3.48
-Enter grade 1: 89
-Enter grade 2: 99
-Enter grade 3: 100
-Enter grade 4: 87
+Enter assignment grade 1: 89
+Enter assignment grade 2: 99
+Enter assignment grade 3: 100
+Enter assignment grade 4: 87
+Enter test grade 1: 79
+Enter test grade 2: 89
+Enter test grade 3: 76
+Enter test grade 4: 100
 
-**********************************
-*          John Brown            *
-**********************************
+**************************************************
+*                  John Brown                    *
+**************************************************
 Gpa: 3.48
 Grades: 89, 99, 100, 87
-Min grade: 87
-Max grade: 100
-Average grade: 93.75
-**********************************
+Min assignment grade: 87
+Min test grade: 76
+Average assignment grade: 93.75
+Average assignment grade (without lowest): 96.00
+Final assignment weighted average: 93.70
+Average test grade: 86.00
+Average test grade (without lowest): 89.33
+Final test weighted average: 86.40
+**************************************************
+
 
 """
 
-"""
-Challenge: Alter your output to display this way (ie., additional * characters)
+# TODO -> add your code here
 
-**********************************
-*          John Brown            *
-**********************************
-* Gpa: 3.48                      *
-* Grades: 89, 99, 100, 87        *
-* Min grade: 87                  *
-* Max grade: 100                 *
-* Average grade: 93.75           *
-**********************************
+
+
+
+
+
+
+"""
+Challenge: Alter your output to display this way (ie., additional * characters for each row)
+
+**************************************************
+*                  John Brown                    *
+**************************************************
+* Gpa: 3.48                                      *
+* Grades: 89, 99, 100, 87                        *
+
+                     etc...
+
+**************************************************
 
 """
 
